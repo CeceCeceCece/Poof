0476367ac0e8acb42b2f2055fdf83bb29a973c64
Czeglédi Levente <czevente@gmail.com>
2021-09-10 00:18:18 +0200

    realtime database working as a charm

--- flutter/poof/lib/main.dart
+++ flutter/poof/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_performance/firebase_performance.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
@@ -15,15 +16,26 @@ void _firebaseTesting() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
+  DatabaseReference testRef = FirebaseDatabase(
+          databaseURL:
+              "https://poof-8e0bc-default-rtdb.europe-west1.firebasedatabase.app")
+      .reference()
+      .child('test');
+
+  final dbResult = await testRef.get();
+
+  print('Database entries:  ${dbResult.value}');
+
+  testRef.set("testvalue").then((value) => print('database entry added'));
+
   final analytics = FirebaseAnalytics();
   analytics.logEvent(name: "analytics_working");
 
   _performanceCustomTrace();
 
-  await FirebaseCrashlytics.instance
-      .recordFlutterError(FlutterErrorDetails(exception: "exception"));
-
-  // ! A következő két sor crashelné az alkalmazást, ezekről instant report érekzik firebase-re
+  // ! A következő pár sor crashelné az alkalmazást, ezekről instant report érekzik firebase-re
+  // await FirebaseCrashlytics.instance
+  //    .recordFlutterError(FlutterErrorDetails(exception: "exception"));
   //throw FlutterError("error");
   //FirebaseCrashlytics.instance.crash(); működik
 }
