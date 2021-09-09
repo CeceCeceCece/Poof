41011416cf7e37fb1052621b76640e4d5c7e3d0e
Czeglédi Levente <czevente@gmail.com>
2021-09-10 00:04:54 +0200

    performance, emulators, crashlytics and app distribution wired into flutter project

--- flutter/poof/lib/main.dart
+++ flutter/poof/lib/main.dart
@@ -1,15 +1,36 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_performance/firebase_performance.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  firebaseTesting();
+  _firebaseTesting();
 
   runApp(App());
 }
 
-void firebaseTesting() async {
+void _firebaseTesting() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   final analytics = FirebaseAnalytics();
   analytics.logEvent(name: "analytics_working");
+
+  _performanceCustomTrace();
+
+  await FirebaseCrashlytics.instance
+      .recordFlutterError(FlutterErrorDetails(exception: "exception"));
+
+  // ! A következő két sor crashelné az alkalmazást, ezekről instant report érekzik firebase-re
+  //throw FlutterError("error");
+  //FirebaseCrashlytics.instance.crash(); működik
+}
+
+void _performanceCustomTrace() async {
+  final trace = FirebasePerformance.instance.newTrace('myCustomTrace');
+  trace.start();
+  await Future.delayed(Duration(seconds: 5));
+  trace.stop();
 }
