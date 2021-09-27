0a66982ffa2a9989b73c9c1dba99e9dc7550ed30
Czeglédi Levente <czevente@gmail.com>
2021-09-27 20:36:23 +0200

    improved login screen by skeleton layout

--- flutter/poof/lib/pages/login/login_controller.dart
+++ flutter/poof/lib/pages/login/login_controller.dart
@@ -1,11 +1,20 @@
+import 'package:bang/routes/routes.dart';
 import 'package:bang/services/audio_service.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
+  final usernameC = TextEditingController();
+  final passwordC = TextEditingController();
+
+  void login() {
+    Get.offAndToNamed(Routes.HOME);
+  }
+
   @override
   void onInit() {
     AudioService.playMenuSong();
-    //AudioService.playBackgroundMusic();
+
     super.onInit();
   }
 }
--- flutter/poof/lib/pages/login/login_view.dart
+++ flutter/poof/lib/pages/login/login_view.dart
@@ -4,12 +4,46 @@ import 'package:get/get.dart';
 import 'login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("login screen"),
-      ),
+    final userField = TextField(controller: controller.usernameC);
+    final passwordField = TextField(
+      controller: controller.passwordC,
+      obscureText: true,
     );
+
+    return Form(
+        key: _formKey,
+        child: Scaffold(
+          body: SingleChildScrollView(
+            child: Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width - 60,
+                child: Column(
+                  children: [
+                    SizedBox(height: 150),
+                    Text('BANG!'),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    userField,
+                    SizedBox(
+                      height: 10,
+                    ),
+                    passwordField,
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: controller.login,
+                      style: ElevatedButton.styleFrom(),
+                      child: Text('Login'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
