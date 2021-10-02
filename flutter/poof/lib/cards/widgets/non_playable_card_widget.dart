dbc9e9e64899d3f9ec4f858deeedfadf88745417
Czeglédi Levente <czevente@gmail.com>
2021-10-03 00:30:57 +0200

    added some packages, notification package needs to be integrated properly

--- flutter/poof/lib/cards/widgets/non_playable_card_widget.dart
+++ flutter/poof/lib/cards/widgets/non_playable_card_widget.dart
@@ -6,7 +6,7 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:screenshot/screenshot.dart';
-import 'package:share/share.dart';
+import 'package:share_plus/share_plus.dart';
 
 import 'card_widget_helpers.dart';
 
--- flutter/poof/lib/pages/login/login_view.dart
+++ flutter/poof/lib/pages/login/login_view.dart
@@ -1,4 +1,5 @@
 import 'package:animated_icon_button/animated_icon_button.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
@@ -69,6 +70,36 @@ class LoginView extends GetView<LoginController> {
                         ),
                       ],
                     ),
+                    SizedBox(
+                      width: 250.0,
+                      child: DefaultTextStyle(
+                        style: const TextStyle(
+                          fontSize: 35,
+                          color: Colors.white,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 7.0,
+                              color: Colors.white,
+                              offset: Offset(0, 0),
+                            ),
+                          ],
+                        ),
+                        child: AnimatedTextKit(
+                          repeatForever: true,
+                          animatedTexts: [
+                            FlickerAnimatedText('Flicker Frenzy',
+                                textStyle: TextStyle(color: Colors.amber)),
+                            FlickerAnimatedText('Night Vibes On',
+                                textStyle: TextStyle(color: Colors.amber)),
+                            FlickerAnimatedText("C'est La Vie !",
+                                textStyle: TextStyle(color: Colors.amber)),
+                          ],
+                          onTap: () {
+                            print("Tap Event");
+                          },
+                        ),
+                      ),
+                    )
                   ],
                 ),
               ),
