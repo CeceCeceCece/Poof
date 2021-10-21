a8b507d2c44c5a7b2d3e7772c3645de073104ac0
Csabi1998 <61312438+Csabi1998@users.noreply.github.com>
2021-10-21 12:22:50 +0200

    services

---
+++ dotnet/PoofBackend/Application/Services/ConnectionService.cs
@@ -0,0 +1,34 @@
+﻿using Application.Interfaces;
+using Domain;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class ConnectionService : IConnectionService
+    {
+        private readonly PoofDbContext context;
+
+        public ConnectionService(PoofDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Connection> GetConnection(string conncetionId, CancellationToken cancellationToken = default)
+        {
+            return await context.Connections.FirstOrDefaultAsync(x => x.ConnectionId == conncetionId, cancellationToken);
+        }
+
+        public async Task RemoveConnection(Connection connection, CancellationToken cancellationToken = default)
+        {
+            context.Connections.Remove(connection);
+            await context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ dotnet/PoofBackend/Application/Services/LobbyService.cs
@@ -0,0 +1,46 @@
+﻿using Application.Constants;
+using Application.Exceptions;
+using Application.Interfaces;
+using Domain;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class LobbyService : ILobbyService
+    {
+        private readonly PoofDbContext context;
+
+        public LobbyService(PoofDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task CreateLobby(Lobby lobby, CancellationToken cancellationToken)
+        {
+            var hasLobby = await context.Lobbies.AnyAsync(x => x.Name == lobby.Name || x.Vezeto == lobby.Vezeto, cancellationToken);
+
+            if (hasLobby)
+                throw new PoofException(LobbyMessages.NEV_MAR_LETEZIK);
+
+            await context.Lobbies.AddAsync(lobby, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
+        }
+
+        public Task<Lobby> GetLobby(string id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveLobby(string id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
