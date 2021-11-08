24dcf0eecd24ce241f4b8ddaf865f98f5f45b130
Czeglédi Levente <czevente@gmail.com>
2021-11-08 17:15:45 +0100

    qr code scanning moved to home page controller

--- flutter/poof/lib/pages/home/home_controller.dart
+++ flutter/poof/lib/pages/home/home_controller.dart
@@ -2,9 +2,16 @@ import 'package:bang/routes/routes.dart';
 import 'package:bang/services/audio_service.dart';
 import 'package:bang/services/game_service.dart';
 import 'package:bang/services/shared_preference_service.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class HomeController extends GetxController {
+  final GlobalKey _qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+
   void joinRoom(String roomId) {
     Get.toNamed(Routes.GAME);
     AudioService.playBackgroundMusic();
@@ -14,5 +21,55 @@ class HomeController extends GetxController {
   void logout() {
     SharedPreferenceService.token = '';
     Get.offAndToNamed(Routes.LOGIN);
+  }
+
+  void readQR() {
+    Get.defaultDialog(
+        content: Container(
+      width: 300,
+      height: 300,
+      child: QRView(
+        key: _qrKey,
+        onQRViewCreated: _onQRViewCreated,
+      ),
+    ));
+  }
+
+  void showQR() {
+    Get.defaultDialog(
+      content: Container(
+        width: 250,
+        height: 250,
+        child: QrImage(
+          data: "QR READ COMPLETED!",
+          version: QrVersions.auto,
+          errorCorrectionLevel: QrErrorCorrectLevel.Q,
+          size: 100.0,
+          embeddedImage: AssetImage(
+            'assets/icons/bang_logo.png',
+          ),
+          embeddedImageStyle: QrEmbeddedImageStyle(size: Size(30, 30)),
+        ),
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream
+        .listen((scanData) => _onQRReadSuccessfully(scanData.code));
+  }
+
+  void _onQRReadSuccessfully(String qrValue) {
+    Get.back();
+    controller?.dispose();
+    Fluttertoast.showToast(
+        msg: "QR Read: $qrValue",
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
   }
 }
--- flutter/poof/lib/pages/login/login_controller.dart
+++ flutter/poof/lib/pages/login/login_controller.dart
@@ -1,10 +1,7 @@
 import 'package:bang/routes/routes.dart';
 import 'package:bang/services/auth_service.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 
 class LoginController extends GetxController {
   final usernameC = TextEditingController(text: 'Cece');
@@ -12,9 +9,6 @@ class LoginController extends GetxController {
   final confirmPasswordC = TextEditingController(text: 'admin');
 
   var isLoginPage = true.obs;
-
-  final GlobalKey _qrKey = GlobalKey(debugLabel: 'QR');
-  QRViewController? controller;
 
   void login() {
     Get.find<AuthService>().login(usernameC.text, passwordC.text);
@@ -37,55 +31,5 @@ class LoginController extends GetxController {
     usernameC.clear();
     passwordC.clear();
     confirmPasswordC.clear();
-  }
-
-  void readQR() {
-    Get.defaultDialog(
-        content: Container(
-      width: 300,
-      height: 300,
-      child: QRView(
-        key: _qrKey,
-        onQRViewCreated: _onQRViewCreated,
-      ),
-    ));
-  }
-
-  void showQR() {
-    Get.defaultDialog(
-      content: Container(
-        width: 250,
-        height: 250,
-        child: QrImage(
-          data: "QR READ COMPLETED!",
-          version: QrVersions.auto,
-          errorCorrectionLevel: QrErrorCorrectLevel.Q,
-          size: 100.0,
-          embeddedImage: AssetImage(
-            'assets/icons/bang_logo.png',
-          ),
-          embeddedImageStyle: QrEmbeddedImageStyle(size: Size(30, 30)),
-        ),
-      ),
-    );
-  }
-
-  void _onQRViewCreated(QRViewController controller) {
-    this.controller = controller;
-    controller.scannedDataStream
-        .listen((scanData) => _onQRReadSuccessfully(scanData.code));
-  }
-
-  void _onQRReadSuccessfully(String qrValue) {
-    Get.back();
-    controller?.dispose();
-    Fluttertoast.showToast(
-        msg: "QR Read: $qrValue",
-        toastLength: Toast.LENGTH_LONG,
-        gravity: ToastGravity.BOTTOM,
-        timeInSecForIosWeb: 1,
-        backgroundColor: Colors.red,
-        textColor: Colors.white,
-        fontSize: 16.0);
   }
 }
